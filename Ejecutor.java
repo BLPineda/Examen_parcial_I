79abf2f9ab108921c350143461f02ba6da7518ad
BLPineda <84738704+BLPineda@users.noreply.github.com>
2022-02-26 10:22:05 -0600

    Primer parcial

---
+++ Ejecutor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ParcialI;
+
+/**
+ *
+ * @author lenin
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ordenes ordenes = new Ordenes();
+        
+        ordenes.pedir_numero();
+        ordenes.menu(0);
+    }
+    
+}
